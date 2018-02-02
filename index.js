30d28219d9c1afc07197c9ba7e76ad4bb3f8a491
An Nuyen <annuyen@gmail.com>
2018-02-02 00:09:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+const produceDrivingRange = function (){
+  return function()
+}
+
+const produceTipCalculator = function(){
+  return 
+}
+
+const createDriver = function(){
+  let DriverId = 0
+  return class{
+    constructor(){
+      this.name = name 
+      this.id = ++DriverId;
+    }
+    
+  }
+}
